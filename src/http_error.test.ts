659ed37f07c648c58430efa8d5c315e416717f03
Artem Melnyk <melkam464@gmail.com>
2023-07-01 16:52:16 +0200

    tests for new error

--- src/http_error.test.ts
+++ src/http_error.test.ts
@@ -1,42 +1,36 @@
-import { HTTPError, isHTTPError } from "./http_error.ts";
+import { isHTTPError, YumiError } from "./http_error.ts";
 import {
 	assert,
 	assertEquals,
 } from "https://deno.land/std@0.192.0/testing/asserts.ts";
 
-Deno.test("HTTPError - create with json value", async () => {
+Deno.test("YumiError", async () => {
+	const req = new Request("http://example.com");
 	const res = new Response(JSON.stringify({ foo: "bar" }), { status: 500 });
-	const error = await HTTPError.create(res);
+	const error = await YumiError.create(req, res);
 
-	assertEquals(error.json, { foo: "bar" });
-	assertEquals(error.text, JSON.stringify({ foo: "bar" }));
+	assertEquals(error.body, { foo: "bar" });
 	assert(error.status === 500);
+	assert(error.url === res.url);
+	assertEquals(error.request, req);
 	assertEquals(error.response, res);
 });
 
-Deno.test("HTTPError - create with text", async () => {
-	const res = new Response("Bad request", { status: 400 });
-	const error = await HTTPError.create(res);
-
-	assert(typeof error.json === "undefined");
-	assertEquals(error.text, "Bad request");
-	assert(error.status === 400);
-	assertEquals(error.response, res);
-});
-
-Deno.test("HTTPError - without body", async () => {
+Deno.test("YumiError - without body", async () => {
+	const req = new Request("http://example.com");
 	const res = new Response(null, { status: 400 });
-	const error = await HTTPError.create(res);
+	const error = await YumiError.create(req, res);
 
-	assert(typeof error.json === "undefined");
-	assert(typeof error.text === "undefined");
+	assert(typeof error.body === "undefined");
 	assert(error.status === 400);
+	assertEquals(error.request, req);
 	assertEquals(error.response, res);
 });
 
 Deno.test("isHTTPError with HTTPError", async () => {
-	assert(
-		isHTTPError(await HTTPError.create(new Response(null, { status: 400 }))),
-	);
+	const req = new Request("http://example.com");
+	const res = new Response(null, { status: 400 });
+
+	assert(isHTTPError(await YumiError.create(req, res)));
 	assert(isHTTPError(new Error("Not a HTTPError")) === false);
 });
